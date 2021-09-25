13b4d7e5bf56a1c9cad25aafe42af88120eb45ec
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-09-25 01:14:38 -0700

    Removed comments

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Home from './pages/Home';
 import News from './pages/News';
 import { Header, Footer } from './components/common';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -16,22 +15,6 @@ function App() {
       </Switch>
       <Footer/>
     </Router>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
