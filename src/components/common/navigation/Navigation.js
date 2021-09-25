3b1101742bf75846167f6815c5dd42099317d75b
Samantha Fok <70719415+samamoo@users.noreply.github.com>
2021-09-25 00:35:20 -0700

    Added new paths to navbar

--- src/components/common/navigation/Navigation.js
+++ src/components/common/navigation/Navigation.js
@@ -4,9 +4,9 @@ import './Navigation.scss';
 export default function Navigation() {
   return(
     <section className="navigation">
-      <a href="#" className="navigation-item">Home</a>
+      <a href="/" className="navigation-item">Home</a>
       <a href="#" className="navigation-item">Our Story</a>
-      <a href="#" className="navigation-item">News & Updates</a>
+      <a href="/news" className="navigation-item">News & Updates</a>
       <a href="#" className="navigation-item">Contact Us</a>
     </section>
   )
